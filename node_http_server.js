bb85f004f7bf9bffb16d3d7bee2526bf02747d04
Sasha Goldshtein <goldshtn@gmail.com>
2012-07-02 11:37:05 -0400

    semicolon

--- node_http_server.js
+++ node_http_server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>Hello, World</h1>');
-})
+});
 server.listen(process.env.PORT);
